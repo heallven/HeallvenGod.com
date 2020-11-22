565a939cd1c43c3e3a84f0402bc426b4d6d82af0
heallven <74813401+heallven@users.noreply.github.com>
2020-11-22 15:43:28 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+<script>
+
+ var modal = document.getElementById('id01');
+window.onclick = function(event) {
+ if (event.target == modal) {
+  modal.style.display = "none";
+ }
+}
+
+
+var =
+function checkPassword(word) {
+ if (word === "") {
+  console.log("Logging in");
+ } else {
+  console.log("Wrong password");
+ }
+}
+
+checkPassword("");
+
+// Get the modal
+
+
+// When the user clicks anywhere outside of the modal, close it
+</script>
